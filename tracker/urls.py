ea92866553289a3e98cb0e3467372cb12caaa06c
Prateek Sehgal <63642117+prateek6542@users.noreply.github.com>
2024-12-09 13:48:16 +0530

    Update urls.py

--- tracker/urls.py
+++ tracker/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.dashboard, name='dashboard'),
     path('log_meal/', views.log_meal, name='log_meal'),
-    path('add_food_item/', views.add_food_item, name='add_food_item'),
+    path('add_food_items/', views.add_food_item, name='add_food_item'),
     path('daily-log/', views.daily_log, name='daily_log'),
     path('delete-log/<int:log_id>/', views.delete_log, name='delete_log'),
 
